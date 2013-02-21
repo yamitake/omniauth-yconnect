6539e36e1fd82a4d175cce7df9bba6380881ed02
jun <take.yapr@gmail.com>
2013-02-21 15:37:45 +0900

    test

--- lib/omniauth/strategies/yconnect.rb
+++ lib/omniauth/strategies/yconnect.rb
@@ -51,7 +51,10 @@ module OmniAuth
         options.token_params = {} if options.token_params.nil?
         verifier = request.params['code']
         params = {:redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true))
-        params[:headers] = {"HTTP_AUTHORIZATION" => 'Basic ' + Base64::encode64("#{options.client_id}:#{options.client_secret}").strip}
+        params[:headers] = {'HTTP_AUTHORIZATION' => 'Basic ' + Base64::encode64("#{options.client_id}:#{options.client_secret}").strip ,
+                            'Content-Type'       => 'application/x-www-form-urlencoded'}
+        params.delete "client_id"
+        params.delete "client_secret"
         client.auth_code.get_token(verifier, params , deep_symbolize(options.auth_token_params))
       end
 
